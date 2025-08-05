aec3881e5c0f434c230fa92d5d1a1b2b4242ff2b
Glen Lim <glenlim@Glens-MacBook-Pro.local>
2025-08-05 17:17:31 +0800

    Start work on searchbar component

--- frontend/src/pages/LandingPage/LandingPage.tsx
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,10 +1,14 @@
+import TopBar from "@/pages/LandingPage/components/TopBar"
 function LandingPage() {
   return (
-    <div className="landing-page">
-      <div>Top Header</div>
-      <h1>Welcome to the Landing Page</h1>
-      <p>This is the main entry point of our application.</p>
-    </div>
+    <>
+      <TopBar />
+      <div className="landing-page">
+        <div>Top Header</div>
+        <h1>Welcome to the Landing Page</h1>
+        <p>This is the main entry point of our application.</p>
+      </div>
+    </>
   );
 }
 
--- frontend/src/pages/LandingPage/components/TopBar.tsx
+++ frontend/src/pages/LandingPage/components/TopBar.tsx
@@ -1,8 +1,24 @@
-
+import { InputAdornment, TextField } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 function TopBar() {
   return (
-    <div className="top-bar">
-      <h2>Top Bar</h2>
+    <div className="flex p-4 bg-red-500 text-white rounded-lg">
+      <img src="dota-2-icon.png" className="w-13 h-13" />
+      <div className="flex justify-center items-center w-full">
+        <TextField
+          variant="outlined"
+          slotProps={{
+            input: {
+              sx: {borderRadius: 2},
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              )
+            },
+          }}
+        />
+      </div>
     </div>
   );
 }
