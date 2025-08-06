dd32d3f076826f13d2aa50f6304cada46925da00
Glen Lim <glenlim@Glens-MacBook-Pro.local>
2025-08-06 15:22:33 +0800

    Update colors, Add dynamic scaling to topbar

--- frontend/src/pages/LandingPage/components/TopBar.tsx
+++ frontend/src/pages/LandingPage/components/TopBar.tsx
@@ -2,17 +2,29 @@ import { InputAdornment, TextField } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 function TopBar() {
   return (
-    <div className="flex p-4 bg-red-500 text-white rounded-lg">
+    <div className="flex p-4 bg-[#072f36] rounded-lg">
       <img src="dota-2-icon.png" className="w-13 h-13" />
       <div className="flex justify-center items-center w-full">
         <TextField
           variant="outlined"
+          placeholder="Search for players, teams, or matches"
           slotProps={{
             input: {
-              sx: {borderRadius: 2},
+              sx: { borderRadius: 2, 
+                backgroundColor: '#215059', 
+                color: 'white', 
+                fontSize: {
+                  xs: "0.8rem",  // small devices
+                  md: "1.2rem",  // desktops
+                },
+                width: {
+                  xs: '280px',  // small devices
+                  md: '400px',
+                }, 
+                },
               startAdornment: (
                 <InputAdornment position="start">
-                  <SearchIcon />
+                  <SearchIcon sx={{ color: 'white' }}/>
                 </InputAdornment>
               )
             },
